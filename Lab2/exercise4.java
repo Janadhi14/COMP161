2d57fa34e6a3acb658d464b1fd8d52097b80c824
Janadhi <100277240+Janadhi14@users.noreply.github.com>
2022-04-17 13:40:23 +1200

    Create exercise4.java

---
+++ Lab2/exercise4.java
@@ -0,0 +1,15 @@
+public class exercise4{
+   public static void main(String[ ] args){
+   int i = 50;
+   int x = 12;
+   x = i;
+   i = x;
+		System.out.println(i);
+		System.out.println(x);
+
+
+   }
+  }
+
+//this should print out both values for x and i which will be 50
+//this should print out the comment "2 plus 2 is 4"//
