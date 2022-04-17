c89434ec2fa781e743dd946bbc7588b4de8c76be
Janadhi <100277240+Janadhi14@users.noreply.github.com>
2022-04-17 13:41:58 +1200

    Create quote.java

---
+++ Lab3/quote.java
@@ -0,0 +1,11 @@
+/**write a java program that prints out
+ "Before you marry a person, you should first make them use a computer with slow Internet to see who they really are."
+             - Will Ferrell"*/
+             
+public class quote{
+	public static void main(String [] args) { 
+		System.out.println("\"Before you marry a person, you should first make them use a computer with slow Internet to see who they really are\"");
+      System.out.println(" ");
+      System.out.println("   - Will Ferrell");  
+      }
+}
