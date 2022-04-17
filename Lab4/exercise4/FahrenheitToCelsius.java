cb6ad0fe9266a748809d0afb10ed8af39d2bd3f7
Janadhi <100277240+Janadhi14@users.noreply.github.com>
2022-04-17 14:00:00 +1200

    Create FahrenheitToCelsius.java

---
+++ Lab4/exercise4/FahrenheitToCelsius.java
@@ -0,0 +1,21 @@
+//we need to first import 
+import java.util.Scanner; 
+public class FahrenheitToCelsius {
+    public static void main ( String [ ] args ) {  
+        double x = celsiusToFahrenheit( "Please enter an the tempreature in celsius:" ); //we are able to alter these 2 statements in order to call other methods within the program.
+        System.out.println( "the tempreature in fahrenheit is: " + ((x-32)/1.8) + "degrees.");
+        //double y = fahrenheitToCelsius( "Please enter a tempreature in celsius: "); //mehtod call for fahrenheit
+        //System.out.println( "the tempreature in fahrenheit is: " + ((y*1.8)+32) + "degrees.");
+            
+    }
+        public static double celsiusToFahrenheit( String message ) { 
+        System.out.println( message );
+        Scanner sc = new Scanner( System.in ); 
+        return sc.nextDouble();
+    }
+       public static double fahrenheitToCelsius( String message ) { 
+        System.out.println( message );
+        Scanner sc = new Scanner( System.in ); 
+        return sc.nextDouble();
+    }
+}
