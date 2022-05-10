665f23d8b95eed8391b0bb26e2e8cdcb0dc5770a
Janadhi <100277240+Janadhi14@users.noreply.github.com>
2022-05-10 19:11:14 +1200

    Create CatApp.java

---
+++ lab5/CatApp.java
@@ -0,0 +1,7 @@
+/**  Lab 5 BoxApp.java */
+public class CatApp{
+    public static void main(String [] Args){
+    Cat cat1 =  new Cat("tiddles", "tabby", 7, 'm', true );
+    cat1.displayCat();
+    }
+}
